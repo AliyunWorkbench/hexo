41dd473ca5daa9df4f9a32bde57251115ec4aaf8
LineXic <121679359+LineXic@users.noreply.github.com>
2024-02-15 13:39:52 +0800

    Add files via upload

---
+++ themes/scripts/events/index.js
@@ -0,0 +1,16 @@
+/* global hexo */
+
+'use strict';
+
+hexo.on('generateBefore', () => {
+  require('./lib/merge-configs')(hexo);
+  require('./lib/compatible-configs')(hexo);
+  require('./lib/injects')(hexo);
+  require('./lib/highlight')(hexo);
+  require('./lib/lazyload')(hexo);
+  require('./lib/footnote')(hexo);
+});
+
+hexo.on('generateAfter', () => {
+  require('./lib/hello')(hexo);
+});
